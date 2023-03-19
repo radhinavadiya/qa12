0b6d442335ad221c9196c137a9ffe5567e46c2f1
radhi navadiya <radhinav92@gmail.com>
2023-03-19 14:41:26 +0000

    qa12

---
+++ team QA/src/qa.java
@@ -0,0 +1,15 @@
+public class qa {
+    static int b=20;
+    static int c=10;
+
+    static String input="we are 10 people in QA team. spending £$ 1650";
+    static String input3 = "we are future QA ";
+
+    public static void main(String[] args) {
+        System.out.println( input3);
+
+       // System.out.println(input.replaceAll("[^0-9]", ""));
+
+
+    }
+}
